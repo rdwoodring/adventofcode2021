26f118e12e5fc34e686829e659f0eb04b8c6e89d
Ryan Woodring <rdwoodring@gmail.com>
2021-12-15 22:00:53 -0500

    day13 part one complete and part 2 in progress

---
+++ day13/Point.js
@@ -0,0 +1,8 @@
+class Point {
+    constructor(x, y) {
+        this.x = Number(x);
+        this.y = Number(y);
+    }
+}
+
+export default Point;
---
+++ day13/part2/index.js
@@ -0,0 +1,104 @@
+import {
+    points,
+    folds
+} from '../input.js';
+
+import Point from '../Point.js';
+
+function stringifyPoint(point) {
+    const {
+        x,
+        y
+    } = point;
+
+    return `${x},${y}`;
+}
+
+function doFold(foldInstruction, constructedPoints) {
+    const {
+            axis,
+            coord
+        } = foldInstruction;
+
+    debugger;
+
+    if (axis === 'y') {
+        const pointsToReflect = constructedPoints.filter(point => point.y > coord),
+            pointsSansPointsToReflect = constructedPoints.filter(point => !pointsToReflect.includes(point)),
+            reflectedPoints = pointsToReflect.map(point => {
+                const {
+                    x,
+                    y
+                } = point,
+                delta = y - coord;
+
+                return new Point(x, coord - delta)
+            }),
+            dedupedPoints = Array.from(new Set([...pointsSansPointsToReflect, ...reflectedPoints].map(point => stringifyPoint(point))));
+            
+        return dedupedPoints.map(point => {
+            const [
+                x,
+                y
+            ] = point.split(',');
+
+            return new Point(x, y);
+        });
+    } else {
+        const pointsToReflect = constructedPoints.filter(point => point.x > coord),
+            pointsSansPointsToReflect = constructedPoints.filter(point => !pointsToReflect.includes(point)),
+            reflectedPoints = pointsToReflect.map(point => {
+                const {
+                    x,
+                    y
+                } = point,
+                delta = x - coord;
+
+                return new Point(coord - delta, y);
+            }),
+            dedupedPoints = Array.from(new Set([...pointsSansPointsToReflect, ...reflectedPoints].map(point => stringifyPoint(point))));
+            
+        return dedupedPoints.map(point => {
+            const [
+                x,
+                y
+            ] = point.split(',');
+
+            return new Point(x, y);
+        });
+    }
+}
+
+const constructedPoints = points.map(point => {
+        const [
+            x,
+            y
+        ] = point.split(',');
+
+        return new Point(x, y);
+    }),
+    pointMap = constructedPoints.reduce((acc, point) => {
+        return {
+            ...acc,
+            [stringifyPoint(point)]: point
+        }
+    }, {}),
+    foldInstructions = folds.map(fold => {
+        const [
+            axis,
+            coord
+        ] = fold.replace('fold along ', '')
+            .split('=');
+
+        return {
+            axis,
+            coord: Number(coord)
+        }
+    }),
+    // [firstInstruction] = foldInstructions,
+    // reflectedPoints = doFold(firstInstruction, constructedPoints);
+    reflectedPoints = foldInstructions.reduce((acc, instruction) => {
+        return doFold(instruction, acc);
+    }, constructedPoints);
+
+console.log(reflectedPoints);
